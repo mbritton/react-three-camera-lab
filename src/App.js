64769580d059208f3044f07b92e974af6a5ec8df
Mike Britton <mbritton@oraero.com>
2020-11-22 09:51:32 -0500

    Fix broken build due to new package not showing up for the compiler for some reason.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import Experiment00 from "./experiments/Experiment00";
-import * as experiments from "./experiments";
 import './App.css';
-
-const visibleComponents = Object.entries(experiments);
 
 function App() {
     return (
