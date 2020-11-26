0ec5b57d4ea03db9bbc88b22cefd4821b479f5eb
Mike Britton <mbritton@oraero.com>
2020-11-26 16:43:32 -0500

    Tweaked animation speed and positioning.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
-import Experiment00 from "./experiments/Experiment00";
-import Experiment01 from "./experiments/Experiment01";
 import ExperimentIndex from "./experiments/ExperimentIndex";
 import './App.css';
 
--- src/experiments/Experiment01.js
+++ src/experiments/Experiment01.js
@@ -1,21 +1,20 @@
-import React, { useRef, Suspense } from 'react';
-import { Canvas, useThree, useFrame } from 'react-three-fiber';
-import { useSpring } from 'react-spring';
+import React, { useRef } from 'react';
+import { Canvas, useFrame, useThree } from 'react-three-fiber';
 import create from 'zustand';
-import { PerspectiveCamera, TextureLoader } from 'three';
 import * as THREE from 'three';
+import { PerspectiveCamera, TextureLoader } from 'three';
 import myImage from "../resources/images/up.png";
 
 let myTexture = null;
 
 const useStore = create((set, get) => ({
     currentCamera: new PerspectiveCamera(),
-    currentCameraPosition: new THREE.Vector3(0,0,5),
     currentGL: null,
     currentScene: null,
     currentRenderer: null,
+    originalCameraQuaternion: new THREE.Quaternion(0,0,0,.5),
     selectedQuaternion: new THREE.Quaternion(0,0,0,.5),
-    targetVector: new THREE.Vector3(0,0,0)
+    targetVector: new THREE.Vector3(0,.5,3)
 }));
 
 function PositionCamera() {
@@ -23,12 +22,15 @@ function PositionCamera() {
     const { camera, gl, scene, renderer } = useThree();
     // Will receive change every time targetVector changes
     let myTargetVector = useStore(state => state.targetVector);
+    // Get the  selected object's quaternion
     let dstQ = useStore(state => state.selectedQuaternion);
-    myTargetVector.setZ(myTargetVector.z + 1);
+    // Use offsets to center the object in frame
+    myTargetVector.setZ(myTargetVector.z + .7);
+    // myTargetVector.setX(myTargetVector.x + .08);
 
     useFrame(() => {
         camera.quaternion.slerp(dstQ, .03);
-        camera.position.lerp(myTargetVector, 0.03);
+        camera.position.lerp(myTargetVector, 0.1);
     })
 
     return null;
@@ -44,30 +46,26 @@ function ScreenBox(props) {
         <mesh
             { ...props }
             ref={ mesh }>
-            <boxBufferGeometry attach="geometry" args={ [1.3, 1, .1] }/>
-            <meshBasicMaterial map={ myTexture } attach="material" />
+            <boxBufferGeometry attach="geometry" args={ [1.3, 1, .03] }/>
+            <meshBasicMaterial map={ myTexture } attach="material" transparent />
 
         </mesh>
     );
 }
 
 function Menu() {
+    function onHomeClickHandler() {
+        useStore.setState({ targetVector: new THREE.Vector3(0,.5,3), selectedQuaternion: useStore.getState().originalCameraQuaternion });
+    }
     return (
-      <div className="menu experiment-01"><a onClick={() => {
-          useStore.setState({ targetVector: new THREE.Vector3(0,0,5) });
-      } }>Home</a></div>
+      <div className="menu experiment-01"><a onClick={onHomeClickHandler}>Home</a></div>
     );
 }
 
 function Experiment01() {
 
     const onScreenClickHandler = (e) => {
-        console.log('e.object', e.object.quaternion);
-        // let selectedObjectVector = new THREE.Vector3(e.object.position.x, e.object.position.y, e.object.position.z);
         useStore.setState({ targetVector: new THREE.Vector3().copy(e.object.position), selectedQuaternion: e.object.quaternion });
-
-        const foo = useStore.getState().selectedQuaternion;
-        console.log('foo', foo);
     }
 
     return(
@@ -75,13 +73,11 @@ function Experiment01() {
             <Canvas id="scene-container">
                 <ambientLight />
                 <pointLight position={ [0, 3, -2.39] }/>
-
                 <ScreenBox onClick={onScreenClickHandler} rotation={[0, .3, 0]} position={ [0, 0, -1] } />
                 <ScreenBox onClick={onScreenClickHandler} position={ [2, 2, -4] } />
                 <ScreenBox onClick={onScreenClickHandler} position={ [0, 4, -2] } />
                 <ScreenBox onClick={onScreenClickHandler} position={ [-2, -4, -6] } />
                 <ScreenBox onClick={onScreenClickHandler} rotation={[0, -.3, 0]} position={ [-6, -4, -12] } />
-
                 <PositionCamera />
             </Canvas>
             <Menu />
